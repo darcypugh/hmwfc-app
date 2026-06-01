528ea1cc1f79839b8552707a4b9982421fe3ad0f
darcypugh <63462099+darcypugh@users.noreply.github.com>
2026-06-01 15:55:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -964,7 +964,7 @@ export default function App() {
   }, []);
   
   useEffect(() => {
-  const ALLOWED_EMAILS = [process.env.REACT_APP_ADMIN_EMAIL];
+  const ALLOWED_EMAILS = ["darcypugh@live.com"];
   const unsub = onAuthStateChanged(auth, (user) => {
     if (user && ALLOWED_EMAILS.includes(user.email)) {
       setShowLogin(false);
